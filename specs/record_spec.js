e6489c81e6ae847d7e8870ef4147f25d5c183b01
Craig Lynagh <craiglynagh@gmail.com>
2017-08-31 14:48:37 +0100

    Initial Commit

---
+++ specs/record_spec.js
@@ -0,0 +1,24 @@
+var assert = require("assert")
+var arrayTasks = require("../record.js")
+
+
+
+describe("Record", function(){
+
+  var diamondDogs;
+
+  beforeEach( funcition(){
+    diamondDogs = new Record("David Bowie", "Diamond Dogs", "Rock", 20.00)
+  });
+
+it("Should have an artist", function(){
+  assert.strictEqual(diamondDogs.artist, "David Bowie")
+})
+
+
+
+
+
+
+
+})
